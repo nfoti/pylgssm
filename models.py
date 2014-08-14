64aa3e69a4aa0e1610999ee3ac3f7347f6abe716
Nick Foti <nfoti01@gmail.com>
2014-08-13 17:03:01 -0700

    Added models.py.

---
+++ models.py
@@ -0,0 +1,16 @@
+
+
+class LGSSMBase(object):
+    """ Linear Gaussian state space model base class.
+    """
+
+    def __init__():
+        pass
+
+
+class LGSSMPython(LGSSMBase):
+    """ Python implementation.
+    """
+
+    def __init__():
+        pass
