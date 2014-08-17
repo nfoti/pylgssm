6811db3f78b7f33d88e61733a36db5ee86addb54
Nick Foti <nfoti01@gmail.com>
2014-08-16 17:24:10 -0700

    Finished simple Kalman filter.

--- pylgssm/models.py
+++ pylgssm/models.py
@@ -1,3 +1,6 @@
+from __future__ import division
+
+import numpy as np
 
 
 class LGSSMBase(object):
@@ -12,10 +15,10 @@ class LGSSMBase(object):
         y_t \in \mathbb{R}^p
     """
 
-    def __init__():
+    def __init__(self):
         self._datas = list()
         self._state_dists = list()
-        self._state_seqs = list()
+        self._state_seqs = list()   # These are for sampling
 
     def add_data(self, data):
         self._datas.append(data)
@@ -33,7 +36,7 @@ class LGSSMFixedPython(LGSSMBase):
         Model parameters A, C, Q, R, mu_0, and S_0 are assumed fixed.
     """
 
-    def __init__(A, C, Q=None, R=None, mu_0=None, S_0=None):
+    def __init__(self, A, C, Q=None, R=None, mu_0=None, S_0=None):
         """
             A    : K x K latent transition matrix
             C    : p x K latent to observation matrix
@@ -43,12 +46,13 @@ class LGSSMFixedPython(LGSSMBase):
                    observation)
             S_0  : K x K covariance matrix for initiaion latent state
         """
+        super(LGSSMFixedPython, self).__init__()
         # Put some error checking in here
         self._A = A
         self._C = C
         p, K = C.shape
         self._p = p
-        self._K = shape
+        self._K = K
         self._Q = Q if Q is not None else np.eye(K)
         self._R = R if R is not None else np.eye(p)
         self._mu_0 = mu_0 if mu_0 is not None else np.zeros(K)
@@ -59,13 +63,15 @@ class LGSSMFixedPython(LGSSMBase):
         C = self._C
         Q = self._Q
         R = self._R
+        mu_0 = self._mu_0
+        S_0 = self._S_0
         p, K = C.shape
 
         V = np.random.multivariate_normal(np.zeros(K), Q, size=T)
         W = np.random.multivariate_normal(np.zeros(p), R, size=T)
 
-        X = np.empty(T, K)
-        Y = np.empty(T, p)
+        X = np.empty((T, K))
+        Y = np.empty((T, p))
 
         # Initial state with no observation
         X0 = mu_0 + np.random.multivariate_normal(np.zeros(K), S_0)
@@ -78,33 +84,35 @@ class LGSSMFixedPython(LGSSMBase):
 
         return X, Y
 
-    def kalman_filter():
+    def kalman_filter(self, idx=None):
         """ Follows algorithm of pg. 57 of "Bayesian Filtering and Smoothing.
+            
+            Updates `_state_dists` member variable.
         """
-        p = self.p
-        K = self.K
         C = self._C
         A = self._A
         Q = self._Q
         R = self._R
+        p, K = C.shape
+        mu_0 = self._mu_0
+        S_0 = self._S_0
 
-        datas = self._datas
+        if idx is not None:
+            datas = self._data[idx]
+        else:
+            datas = self._datas
+
         Ts = [d.shape[0] for d in datas]
 
-        kal_ms = list()
-        kal_Ps = list()
-        
-        for data, T in enumerate(zip(datas, Ts)):
-            kal_m = np.empty(T, K)
-            kal_P = np.empty(T, K, K)
+        for i, (data, T) in enumerate(zip(datas, Ts)):
+            kal_m = np.empty((T, K))
+            kal_P = np.empty((T, K, K))
 
-            kal_m[0,:] = self._mu_0
-            kal_P[0,:,:] = self._S_0
+            # Initial prediction step
+            mbar = np.dot(A, mu_0)
+            Pbar = np.dot(A, np.dot(S_0, A.T)) + Q
 
-            for t in xrange(1,T):
-                # Prediction step
-                mbar = np.dot(A, kal_m[t-1,:])
-                Pbar = np.dot(A, np.dot(kal_P[t-1], A.T)) + Q
+            for t in xrange(T):
 
                 # Update step (4.21)
                 y_t = np.atleast_1d(data[t,:])
@@ -114,9 +122,10 @@ class LGSSMFixedPython(LGSSMBase):
                 K_t = np.dot(Pbar, np.dot(C.T, np.linalg.inv(S_t)))
 
                 kal_m[t,:] = mbar + np.dot(K_t, v_t)
-                kal_P[t,:,:] = Pbar + np.dot(K_t, np.dot(S_t, K_t.T))
+                kal_P[t,:,:] = Pbar - np.dot(K_t, np.dot(S_t, K_t.T))
 
-            kal_ms.append(kal_m)
-            kal_Ps.append(kal_P)
+                # Prediction step for next time step depends on t
+                mbar = np.dot(A, kal_m[t,:])
+                Pbar = np.dot(A, np.dot(kal_P[t], A.T)) + Q
 
-    return kal_ms, kal_Ps
+            self._state_dists[i] = (kal_m, kal_P)
