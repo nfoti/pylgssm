3bd72f87593563d20adc8c9cd37c9cf9c36c0d75
Nick Foti <nfoti01@gmail.com>
2014-08-19 11:26:25 -0700

    Added working Kalman FB algorithm.

--- pylgssm/models/LGSSMFixedPython.py
+++ pylgssm/models/LGSSMFixedPython.py
@@ -189,7 +189,7 @@ class LGSSMFixedSarkkaPython(LGSSMFixedPython):
         return smoothing_dists
 
 
-class LGSSMFixedBealPython(LGSSMFixedPython):
+class LGSSMFixedKalmanFBPython(LGSSMFixedPython):
     """ Implements RTS smoother using a forward messages, \alpha(x_t) and
         backward messages, \beta(x_t), so that
         p(x_t | y_1:T) \propto \alpha(x_t)*\beta(x_t).  This is what the rest
@@ -197,16 +197,20 @@ class LGSSMFixedBealPython(LGSSMFixedPython):
         compare against.
     """
 
-    def kalman_filter(self, didx=None):
-        """ Follows algorithm on pg. 57 of "Bayesian Filtering and Smoothing.
+    def _forward_msgs(self, didx=None):
+        """ Follows Algorithm 3 in Section 2.7 of Emily Fox's PhD thesis.
 
             didx: int or iterable of ints, subset of observations sequences to
                   consider.
             
             Returns:
-                filtering_dists : list of tuples, one for each observation
+                alphas : list of tuples, one for each observation
                                   sequence, containing the means and covariances
                                   of the filtering distribution.
+
+
+            The messages are stored in information form so that we can combine
+            them with the backward messages trivially.
         """
         C = self._C
         A = self._A
@@ -226,26 +230,90 @@ class LGSSMFixedBealPython(LGSSMFixedPython):
 
         Ts = [d.shape[0] for d in datas]
 
+        alphas = list()
+
+        Qi = np.linalg.inv(Q)
+        Ri = np.linalg.inv(R)
+        Ai = np.linalg.inv(A)
+
+        for i, (data, T) in enumerate(zip(datas, Ts)):
+            Hs = np.empty((T, K))
+            Ls = np.empty((T, K, K))
+
+            Lf = np.linalg.inv(S_0)
+            th_f = np.linalg.solve(S_0, mu_0)
+
+            M = np.dot(Ai.T, np.dot(Lf, Ai))
+            J = np.dot(M, np.linalg.inv(M + Qi))
+            L = np.eye(K) - J
+
+            for t in xrange(T):
+                Lpred = np.dot(L, np.dot(M, L.T)) + np.dot(J, np.dot(Qi, J.T))
+                th_pred = np.dot(L, np.dot(Ai.T, th_f))
+
+                y = np.squeeze(data[t,:])
+                Lf = Lpred + np.dot(C.T, np.dot(Ri, C))
+                th_f = th_pred + np.dot(C.T, np.dot(Ri, y))
+
+                Hs[t,:] = th_f
+                Ls[t,:,:] = Lf
+
+                M = np.dot(Ai.T, np.dot(Lf, Ai))
+                J = np.dot(M, np.linalg.inv(M + Qi))
+                L = np.eye(K) - J
+            
+            alphas.append((Hs, Ls))
+
+        return alphas
+
+
+    def kalman_filter(self, didx=None, forward_msgs=None):
+        """ Follows algorithm in Section 2.7 of Emily Fox's PhD thesis.
+
+            didx : int or iterable of ints, subset of observations sequences to
+                  consider.
+            forward_msgs : list of tuples, one for each obs. sequence,
+                           containing the information form of the forward
+                           messages.  This function turns these into means and
+                           covariances to pass to the outside world.
+            
+            Returns:
+                filtering_dists : list of tuples, one for each observation
+                                  sequence, containing the means and covariances
+                                  of the filtering distribution.
+        """
+        C = self._C
+        A = self._A
+        Q = self._Q
+        R = self._R
+        p, K = C.shape
+        mu_0 = self._mu_0
+        S_0 = self._S_0
+
+        if forward_msgs is None:
+            alphas = self._forward_msgs(didx)
+
+        if didx is  None:
+            datas = self._datas
+        elif type(didx) is int:
+            datas = self._data[didx]
+        else:
+            # This will barf if didx is not an iterable of ints
+            datas = [self._data[idx] for idx in didx]
+
+        Ts = [d.shape[0] for d in datas]
+
         filter_dists = list()
 
         for i, (data, T) in enumerate(zip(datas, Ts)):
             Mf = np.empty((T, K))
             Pf = np.empty((T, K, K))
 
-            mbar = np.dot(A, mu_0)
-            Pbar = np.dot(A, np.dot(S_0, A.T)) + Q
+            th_f, Lf = alphas[i]
 
             for t in xrange(T):
-                tmp = np.linalg.inv(np.dot(C, np.dot(Pbar, C.T)) + R)
-                K_t = np.dot(Pbar, np.dot(C.T, tmp))
-
-                y = np.squeeze(data[t,:])
-                r = y - np.dot(C, mbar)
-                Mf[t,:] = mbar + np.dot(K_t, r)
-                Pf[t,:,:] = Pbar - np.dot(K_t, np.dot(C, Pbar))
-
-                mbar = np.dot(A, Mf[t,:])
-                Pbar = np.dot(A, np.dot(Pf[t,:,:], A.T)) + Q
+                Pf[t,:,:] = np.linalg.inv(Lf[t,:,:])
+                Mf[t,:] = np.dot(Pf[t,:,:], th_f[t,:])
 
             filter_dists.append((Mf, Pf))
 
@@ -284,38 +352,54 @@ class LGSSMFixedBealPython(LGSSMFixedPython):
 
         backward_msgs = list()
 
+        Qi = np.linalg.inv(Q)
+        Ri = np.linalg.inv(R)
+        Ai = np.linalg.inv(A)
+
         for i, (data, T) in enumerate(zip(datas, Ts)):
-            Mb = np.empty((T, K))
-            Pb = np.empty((T, K, K))
+            
+            Hs = np.empty((T, K))
+            Ls = np.empty((T, K, K))
 
-            Mb[-1,:] = np.zeros_like(mu_0)
-            Pbi[-1,:,:] = np.zeros_like(S_0)
+            Hs[-1,:] = np.zeros(K)
+            Ls[-1,:,:] = np.zeros((K,K))
+            
+            Lb = np.zeros((K,K))
+            th_b = np.zeros(K)
 
-            for t in reversed(xrange(1,T)):
-                y_t = np.atleast_2d(data[t,:])
-                Psi = np.linalg.inv(Qinv + np.dot(C.T, np.dot(Rinv, C)) + Pbi[t,:,:])
-                Pbi[t-1,:,:] = np.dot(A, np.dot(Qinv, A)) \
-                             - np.dot(A.T, np.dot(Psi, A))
-                m = np.dot(C.T, np.dot(Rinv, y_t))
-                mu = np.dot(Pbi[t,:,:], Mb[t,:])
-                tmp = np.dot(A.T, np.dot(Psi, m + mu))
-                Mb[t-1,:] = np.linalg.solve(Pbi[t-1,:,:], tmp)
+            AtQi = np.dot(A.T, Qi)
+            QiA = np.dot(Qi, A)
+            CtRi = np.dot(C.T, Ri)
+            CtRiC = np.dot(CtRi, C)
 
-            backward_msgs.append((Mb, Pbi))
+            for t in reversed(xrange(T-1)):
+                y = np.squeeze(data[t,:])
+                r = np.dot(CtRi, y) + th_b
+                tmp = np.linalg.inv(Qi + CtRiC + Lb)
+
+                Lb = np.dot(AtQi, A) - np.dot(AtQi, np.dot(tmp, QiA))
+                th_b = np.dot(AtQi, np.dot(tmp, r))
+
+                Hs[t,:] = th_b
+                Ls[t,:,:] = Lb
+
+            backward_msgs.append((Hs, Ls))
 
         return backward_msgs
 
-    def rts_smoother(self, didx=None, filter_dists=None, backward_msgs=None):
+    def rts_smoother(self, didx=None, forward_msgs=None, backward_msgs=None):
         """ Follows algorithm on pg. 57 of "Bayesian Filtering and Smoothing.
 
             didx : int or iterable of ints, subset of observations sequences to
                   consider.
-            filter_dists : list of filtering distributions for the observation
-                           sequences (as produced by `kalman_filter` above).
-                           If not provided `kalman_filter` is called.
+            forward_msgs : list of forward messages for the observation
+                           sequences (as produced by `_forward_msgs` above).
+                           If not provided `kalman_filter` is called.  These
+                           messages are stored in information form.
             backward_msgs : list of backward messages for the observations
                             sequences (as produced by `_backward_msgs` above).
-                            If not provided `_backward_msgs` is called.
+                            If not provided `_backward_msgs` is called.  These
+                            messages are stored in information form.
 
             Returns:
             
@@ -323,13 +407,8 @@ class LGSSMFixedBealPython(LGSSMFixedPython):
                               sequence, containing the means and covariances of
                               the smoothing distributions.
         """
-        C = self._C
-        A = self._A
-        Q = self._Q
-        R = self._R
-        p, K = C.shape
-        mu_0 = self._mu_0
-        S_0 = self._S_0
+        
+        p, K = self._C.shape
 
         if didx is  None:
             datas = self._datas
@@ -339,11 +418,10 @@ class LGSSMFixedBealPython(LGSSMFixedPython):
             # This will barf if didx is not an iterable of ints
             datas = [self._data[idx] for idx in didx]
 
-        # `kalman_filter` handles didx properly
-        if filter_dists is None:
-            filter_dists = self.kalman_filter(didx)
+        if forward_msgs is None:
+            alphas = self._forward_msgs(didx)
         if backward_msgs is None:
-            backward_msgs = self._backward_msgs(didx)
+            betas = self._backward_msgs(didx)
 
         Ts = [d.shape[0] for d in datas]
 
@@ -353,18 +431,17 @@ class LGSSMFixedBealPython(LGSSMFixedPython):
             Ms = np.empty((T, K))
             Ps = np.empty((T, K, K))
             
-            Mf = filter_dists[i][0]
-            Pf = filter_dists[i][1]
+            Hf, Lf = alphas[i]
+            Hb, Lb = betas[i]
 
-            Mb = backward_msgs[i][0]
-            Pbi = backward_msgs[i][1]
+            # Multiplying Gaussians in formation form is adding parameters
+            Hs = Hf + Hb
+            Ls = Lf + Lb
 
             for t in xrange(T):
-                Pfinv = np.linalg.inv(Pf[t,:,:])
-                tmp = np.dot(Pfinv, Mf[t,:]) + np.dot(Pbi[t,:,:], Mb[t,:])
-
-                Ps[t,:,:] = np.linalg.inv(Pfinv + Pbi[t,:,:])
-                Ms[t,:] = np.dot(Ps[t,:,:], tmp)
+                
+                Ps[t,:,:] = np.linalg.inv(Ls[t,:,:])
+                Ms[t,:] = np.dot(Ps[t,:,:], np.squeeze(Hs[t,:]))
 
             smoothing_dists.append((Ms, Ps))
 
